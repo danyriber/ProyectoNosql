5ffb355f52f73db7ffbc1386719f69d548df228d
danyriber <danyriber3232@gmail.com>
2026-02-27 18:49:07 +0100

    quitando fallos

--- routes/movie.routes.js
+++ routes/movie.routes.js
@@ -1,21 +1,21 @@
 const express = require('express');
-const Movie = require('../models/Movie');
+const Movie = require('../models/Movie'); //se requiere express porque cada archivo de rutas necesita express
 const router = express.Router();
 
-// OBTENER TODAS LAS PELICULAS
+//aqui creamos las rutas para todas las peliculas con router y se organizan
+
 router.get('/', async (req, res) => {
 try {
 const movies = await Movie.find();
-return res.status(200).json(movies);
+return res.status(200).json(movies); //para ver todas las peliculas
 } catch (err) {
-return res.status(500).json(err);
+return res.status(500).json(err); //si no las encuentra salta error 500 (internal server error)
 }
 });
 
-// BUSCAR POR ID
 router.get('/id/:id', async (req, res) => {
 try {
-const movie = await Movie.findById(req.params.id);
+const movie = await Movie.findById(req.params.id);  //para buscarlas por id
 if (movie) {
 return res.status(200).json(movie);
 } else {
@@ -26,33 +26,33 @@ return res.status(500).json(err);
 }
 });
 
-// CREAR PELICULA
+
 router.post('/', async (req, res) => {
 try {
 const newMovie = new Movie(req.body);
-const createdMovie = await newMovie.save();
+const createdMovie = await newMovie.save(); //para crear la peli
 return res.status(201).json(createdMovie);
 } catch (err) {
 return res.status(500).json(err);
 }
 });
 
-// EDITAR PELICULA
+
 router.put('/:id', async (req, res) => {
 try {
 const { id } = req.params;
-const movieUpdated = await Movie.findByIdAndUpdate(id, req.body, { new: true });
+const movieUpdated = await Movie.findByIdAndUpdate(id, req.body, { new: true }); //para actualizar la peli
 return res.status(200).json(movieUpdated);
 } catch (err) {
 return res.status(500).json(err);
 }
 });
 
-// BORRAR PELICULA
+
 router.delete('/:id', async (req, res) => {
 try {
 await Movie.findByIdAndDelete(req.params.id);
-return res.status(200).json('Pelicula borrada');
+return res.status(200).json('Pelicula borrada'); //para borrar la peli
 } catch (err) {
 return res.status(500).json(err);
 }
